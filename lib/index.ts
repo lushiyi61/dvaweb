4eb99d7ce46cca46940f42cc79767d3ffb9e92c5
suxiaomo <lksuxiaomo@outlook.com>
2023-04-04 17:31:45 +0800

    再次更新

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@
 @File    :   index.ts
 @Time    :   2023/01/30 10:15:39
 @Author  :   路拾遗
-@Version :   2.0.3
+@Version :   2.0.4
 @Contact :   lk920125@hotmail.com
 yarn add axios @rematch/loading @rematch/core  react-redux redux
 ***************************************/
